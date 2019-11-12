562f53b6f60d6317eacde4339007505fba5b8112
yu-jin0 <pakkuncho085@gmail.com>
2019-11-12 10:52:00 +0900

    update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,10 @@
 class MessagesController < ApplicationController
+  before_action :move_to_index, except: :index
   def index
   end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+  
 end
