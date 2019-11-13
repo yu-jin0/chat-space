06f8ffa9990e969af62a3fd7709f51c35e1c25bd
yu-jin0 <pakkuncho085@gmail.com>
2019-11-13 14:40:13 +0900

    group update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :messages
   has_many :group_users
-  has_many :group, through: :group_users
+  has_many :groups, through: :group_users
 end
